a8a18240fdff13b956569329b644e8106b1989e2
Nick Edwards <nickedwards.dev@gmail.com>
2024-12-03 12:33:42 -0800

    Updates to league db and SQL formatting

--- 08_SQL_CRUD/08_07_league_db.sql
+++ 08_SQL_CRUD/08_07_league_db.sql
@@ -1,61 +1,31 @@
 \c week2;
 
-CREATE
-OR
-REPLACE
-TABLE divisions (
+CREATE TABLE divisions (
     id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL
+    name TEXT NOT NULL UNIQUE
 );
 
-CREATE
-OR
-REPLACE
-TABLE teams (
+CREATE TABLE teams (
     id SERIAL PRIMARY KEY,
     city TEXT NOT NULL,
-    name TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     home_color TEXT NOT NULL,
     away_color TEXT,
     division_id INT
-    -- , FOREIGN KEY (division_id) REFERENCES divisions (id)
 );
 
-ALTER TABLE teams
-ADD CONSTRAINT fk_teams_divisions FOREIGN KEY (division_id) REFERENCES divisions (id);
+ALTER TABLE teams 
+    ADD CONSTRAINT fk_teams_divisions FOREIGN KEY (division_id) REFERENCES divisions (id) 
+    ON DELETE SET NULL;
 
-INSERT INTO
-    divisions (name)
-VALUES ('Atlantic'),
-    ('Metropolitan'),
-    ('Pacific'),
-    ('Central');
+INSERT INTO divisions (name) VALUES 
+    ('Atlantic'), ('Metropolitan'), ('Pacific'), ('Central');
 
-INSERT INTO
-    teams (
-        city,
-        name,
-        home_color,
-        away_color,
-        division_id
-    )
-VALUES (
-        'New York',
-        'Islanders',
-        'Royal blue',
-        'White',
-        2
-    ),
-    (
-        'Seattle',
-        'Kraken',
-        'Deep sea blue',
-        'White',
-        3
-    );
+INSERT INTO teams (city, name, home_color, away_color, division_id) VALUES 
+    ('New York', 'Islanders', 'Royal blue', 'White', 2),
+    ('Seattle', 'Kraken', 'Deep sea blue', 'White', 3);
 
-UPDATE divisions
-set
-    name = 'Cosmopolitan'
-WHERE
-    name = 'Metropolitan';
+UPDATE divisions set name = 'Cosmopolitan' 
+    WHERE name = 'Metropolitan';
+
+DELETE FROM divisions WHERE name = 'Cosmopolitan';
