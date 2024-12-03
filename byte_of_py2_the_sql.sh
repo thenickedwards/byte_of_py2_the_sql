6f7300a7a808b6162db51fb932fd61ac6f09ec29
Nick Edwards <nickedwards.dev@gmail.com>
2024-12-03 13:40:59 -0800

    Bash script to spin up env

--- byte_of_py2_the_sql.sh
+++ byte_of_py2_the_sql.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-# . ./byte_of_py2_the_sql.sh     OR source ./byte_of_py2_the_sql.sh
-# By sourcing the script in your current shell instead of executing it in a subshell, the environment changes (like activating the virtual environment) will persist.
-# ./byte_of_py2_the_sql.sh       OR ./byte_of_py2_the_sql.sh stop     OR . ./byte_of_py2_the_sql.sh    OR source ./byte_of_py2_the_sql.sh
+# >> source ./byte_of_py2_the_sql.sh start
+# OR      
+# >> . ./byte_of_py2_the_sql.sh start
+    # Sources script in current shell (instead of executing in a subshell, 
+    # so that environment changes (like activating the virtual environment) will persist
+
+# Load environment variables
+source .env 
+echo "Loading environment variables..."
 
 # Get the first argument, which will be "start" or "stop"
 action="$1"
 
 # Check if the action is "start"
 if [ "$action" = "start" ]; then
-    echo "Activating \033[3mbyte_of_py2_the_sql\033[0m virtual environment..."
-    source /Users/thenickedwards/.virtualenvs/byte_of_py2_the_sql/bin/activate
-    echo "Virtual environment \033[1mbyte_of_py2_the_sql\033[0m activated."
+    echo "Activating \033[3m$VENV_NAME\033[0m virtual environment..."
+    # source /Users/thenickedwards/.virtualenvs/byte_of_py2_the_sql/bin/activate
+    source "$VENV_PATH"
+    echo "Virtual environment \033[1m$VENV_NAME\033[0m activated."
     echo "Starting Docker containers..."
     docker-compose up -d
     echo "Docker containers started."
+    echo "Opening pgAdmin at http://localhost:5433/"
+    sleep 5
+    open http://localhost:5433/
 # Otherwise, assume the action is "stop"
 else
     echo "Stopping Docker containers..."
     docker-compose stop
-    echo "Deactivating virtual environment..."
+    echo "Deactivating $VENV_NAME virtual environment..."
     deactivate
-    echo "Virtual environment deactivated."
+    echo "Virtual environment $VENV_NAME deactivated."
 fi
